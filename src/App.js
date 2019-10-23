ae99b1f3384f1e89c7c24ae725be3f81611632f5
xiehuaqiang <mrxiehuaqiang@163.com>
2019-10-23 19:39:27 +0800

    简单的测试

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 6666,
-    };
-  }
-  render() {
-    return (
-      <button className="square" onClick={()=> { this.setState({value: 'X'})}}>
-        { this.state.value }
-      </button>
-    );
-  }
-}
+import { View } from './pages/view';
+
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Learn React
-        <Square />
-      </header>
+      <View />
     </div>
   );
 }
