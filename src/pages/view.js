24910b1f8ac4eb360605f334648241de6a2f466b
xiehuaqiang <mrxiehuaqiang@163.com>
2019-11-12 11:22:54 +0800

    临时暂存

--- src/pages/view.js
+++ src/pages/view.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import WelcomeForm  from './form';
+import React from 'react'
+import WelcomeForm  from './form'
+import DomRefDemo  from './domRefDemo'
+import ClassRefDemo  from './classRefDemo'
 
 class NumberList extends React.Component { 
   render() {
-    const numbers = this.props.numbers;
+    const numbers = this.props.numbers
     const listItems = numbers.map((number) =>
       <li key={number.toString()}>{number}</li>
-    );
+    )
     return (
       <ul>{listItems}</ul>
     )
@@ -24,11 +26,13 @@ class View extends React.Component {
   }
   
   render() {
-    const numbers = [1, 2, 3, 4, 5];
+    const numbers = [1, 2, 3, 4, 5]
     return (
       <div>
-        <WelcomeForm handleClick={this.handleClick} />
+        <WelcomeForm handleClick={this.handleClick}/>
         <NumberList numbers={numbers}/>
+        <DomRefDemo/>
+        <ClassRefDemo/>
       </div>
     )
   }
